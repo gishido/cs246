dec4d60202214a206a92748703524f56d3c160c2
gishido <theshum@gmail.com>
2017-04-18 01:36:58 -0700

    Java Hello World Initial Push

---
+++ README.md
@@ -0,0 +1,1 @@
+CS 246 Java Base Code
---
+++ week01/src/shumway/HelloWorld.java
@@ -0,0 +1,14 @@
+package shumway;
+
+public class HelloWorld {
+
+    public static void main(String[] args) {
+	    //creating an instance of the class and calling sayHello function
+        new HelloWorld().sayHello();
+    }
+
+    public void sayHello() {
+        //Hello World using println() function
+        System.out.println("Hello World!");
+    }
+}
