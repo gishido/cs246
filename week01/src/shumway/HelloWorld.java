da31b5a4794ab30bfc3f44e2ba96d55c7b10cbe0
gishido <theshum@gmail.com>
2017-04-18 02:09:03 -0700

    added some radius functions for stretch

--- week01/src/shumway/HelloWorld.java
+++ week01/src/shumway/HelloWorld.java
@@ -12,3 +12,32 @@ public class HelloWorld {
         System.out.println("Hello World!");
     }
 }
+
+public class CircleCalculator {
+
+    public float getRadius() {
+        float myRadius = 0;
+
+        return myRadius;
+    }
+
+    public void displayCircumference(float myRadius) {
+        //print circumference displaying by 2 decimal places
+        System.out.println("Nothing yet to see here!");
+    }
+
+    public void displayArea(float myRadius) {
+        //pint either area or circumference, I'm not sure
+        //  , by 2 decimal places
+        System.out.println("Nothing to see here either!");
+    }
+
+    public static void main(String[] args) {
+        CircleCalculator calc = new CircleCalculator();
+        //gets the redius for the user and then uses it
+        // in other parts of the program
+        float radVal = calc.getRadius();
+        calc.displayArea(radVal);
+        calc.displayCircumference(radVal);
+    }
+}
