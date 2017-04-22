fd0d20524f1dfdb3c4e73259a5991f725924c08c
gishido <theshum@gmail.com>
2017-04-21 23:14:07 -0700

    added exception class framwork

---
+++ week01/User/src/shumway/WeakPasswordException.java
@@ -0,0 +1,12 @@
+package shumway;
+
+/**
+ * Created by thesh on 4/21/2017.
+ */
+public class WeakPasswordException extends Exception {
+
+  public WeakPasswordException(String theException) {
+    super(theException);
+  }
+
+}
