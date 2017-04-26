40426f4d78e4f66a0d685016c29497554cf9ea1a
gishido <theshum@gmail.com>
2017-04-25 19:45:35 -0700

    making sure all files are committed to git

---
+++ week02/VacationCalculator/src/shumway/VacationCalculator.java
@@ -0,0 +1,22 @@
+package shumway;
+
+import java.util.List;
+
+public class VacationCalculator {
+
+    public static void main(String[] args) {
+        VacationCalculator vc = new VacationCalculator();
+
+
+    }
+
+    public float calculateVacationCost(Destination dest)
+    {
+      return 0;
+    }
+
+    float tallyExpenses(List<Expense> expenses)
+    {
+      return 0;
+    }
+}
